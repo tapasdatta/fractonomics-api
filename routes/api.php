61599929190d4dec5734703a20986540acae4807
Tapas Datta <tapash.me@gmail.com>
2025-04-24 13:27:20 +0600

    ``` Add root API route returning a simple string ```

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+Route::get("/user", function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware("auth:sanctum");
+
+Route::get("", function () {
+    return "api";
+});
