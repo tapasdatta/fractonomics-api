89f6d298e608b8c1487f47e2182cfe4dd1050bdb
Tapas Datta <tapash.me@gmail.com>
2025-04-25 19:21:10 +0600

    ``` Remove unused core files and routes ```

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    // App\Providers\AppServiceProvider::class,
     \Modules\Asset\Providers\AssetServiceProvider::class,
     \Modules\User\Providers\UserServiceProvider::class,
 ];
