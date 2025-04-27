9834a79ccab719c0077e4109440c441f08dfe1cc
Tapas Datta <tapash.me@gmail.com>
2025-04-27 16:15:26 +0600

    Remove unused Currency import and fix namespace

--- modules/Asset/Database/migrations/2025_04_25_0703440_create_assets_table.php
+++ modules/Asset/Database/migrations/2025_04_25_0703440_create_assets_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 use Modules\Asset\Enums\AssetStatus;
-use Modules\Currency\Enums\Currency;
+use Modules\Asset\Enums\Currency;
 
 return new class extends Migration {
     /**
